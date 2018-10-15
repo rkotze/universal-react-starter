4d85238b49bfc0536879096c994748577998861b
Richard Kotze <richkotze@outlook.com>
2018-10-15 22:46:12 +0100

    Add PORT env var to customise on server

--- entry/server.js
+++ entry/server.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 
 app.get('*', start);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('http://localhost:' + PORT);
 });
