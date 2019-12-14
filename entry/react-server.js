b33aada36078bab67f4e11075feed8c70217768b
Richard Kotze <richkotze@outlook.com>
2019-12-14 17:41:24 +0000

    React router updated

--- entry/react-server.js
+++ entry/react-server.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import StaticRouter from 'react-router-dom/StaticRouter';
-import { renderRoutes } from 'react-router-config';
-import Helmet from 'react-helmet';
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import { renderRoutes } from "react-router-config";
+import Helmet from "react-helmet";
+const StaticRouter = require("react-router-dom").StaticRouter;
 
-import routes from '../src/routes';
+import routes from "../src/routes";
 
 const start = (req, res) => {
   let context = {};
@@ -29,8 +29,7 @@ const start = (req, res) => {
     </body>
   </html>`;
 
-  if (context.status === 404)
-    res.status(404);
+  if (context.status === 404) res.status(404);
 
   res.send(html);
 };
